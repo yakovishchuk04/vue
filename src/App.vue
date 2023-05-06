582e2643fb477b230e957be3797dc773336c042b
yakovishchuk04 <nevermoreregret@gmail.com>
2023-05-06 11:32:12 +0300

    modified:   package-lock.json 	modified:   package.json 	modified:   public/index.html 	modified:   src/App.vue 	new file:   src/assets/articles-1.png 	new file:   src/assets/articles-2.png 	new file:   src/assets/featured-1.jpg 	new file:   src/assets/featured-2.jpg 	new file:   src/assets/featured-3.jpg 	new file:   src/assets/footer-1.png 	new file:   src/assets/footer-2.png 	new file:   src/assets/footer-3.png 	new file:   src/assets/footer-4.png 	new file:   src/assets/footer-5.png 	new file:   src/assets/header-btn.png 	new file:   src/assets/header-img.png 	modified:   src/assets/logo.png 	new file:   src/assets/rec-1.png 	new file:   src/assets/star.png 	new file:   src/assets/teach-1.png 	new file:   src/components/HeaderApp.vue 	deleted:    src/components/HelloWorld.vue 	modified:   src/main.js

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,88 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+	<div id="app">
+		<HeaderApp />
+	</div>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
 
+<script>
+import HeaderApp from '@/components/HeaderApp.vue'
 export default {
   name: 'App',
   components: {
-    HelloWorld
+		HeaderApp
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+@import url('https://fonts.googleapis.com/css2?family=Source+Sans+Pro:wght@400;700&display=swap');
+	*,
+	*::after,
+	*::before{ 
+	}
+	#app{
+    font-family: 'Source Sans Pro', sans-serif;
+    font-size: 22px;
+    line-height: 33px;
+    color: #F6F6F8;
+	}
+	a{
+    text-decoration: none;
+    color: inherit;
+	}
+	ul{
+		padding-inline-start: 0px;
+	}
+	li{
+		list-style: none;
+	}
+
+.container {
+    max-width: 1296px;
+    margin: 0 auto;
+    padding: 0 10px;  
 }
+.default-title{
+    font-size: 44px;
+    font-weight: 700;
+    line-height: 52.8px;
+}
+.default-line{
+    width: 100%;
+    max-width: 150px;
+    height: 2px;
+    background-color: #57D9A6;
+
+}
+.default-author {
+    font-size: 17px;
+    line-height: 27px;
+    color: #4E4E65;
+}
+.default-btn {
+    font-size: 18px;
+    line-height: 27px;
+    letter-spacing: 1px;
+    color: #1C1C33;
+    background-color: #57D9A6;
+    padding: 16px 24px;
+    border-radius: 4px;
+    text-transform: uppercase;
+    cursor: pointer;
+    transition: all .5s;
+}
+.default-learn-more {
+    text-transform: uppercase;
+    font-size: 16px;
+    line-height: 20.8px;
+    color:#57D9A6;
+    letter-spacing: 1px;
+    transition: all .6s;
+}
+.default-learn-more:hover{
+    color:#23233B;
+}
+
+
 </style>
